f7d56e2613348dd9a1efb8a5738673fd8621bc76
samaviyanoor <gemsreborn@gmail.com>
2026-03-16 03:28:52 -0700

    Add files via upload

---
+++ addition.java
@@ -0,0 +1,16 @@
+class addition {
+    public static void main(String[] args) {
+        
+        
+        int x = 10;
+        int y = 5;
+
+        
+        int z = x + y;
+
+      
+        System.out.println("the value of x is: " + x);
+        System.out.println("the value of y is: " + y);
+        System.out.println("the sum z is: " + z);
+    }
+}
